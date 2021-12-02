147b3a19e76459f69c8b5c6c903bf26d3f804183
arspauld <ars0043@uah.edu>
2021-12-02 13:40:23 -0600

    Implement Matlab Signal Processing & Begin Arduino DSP
    Signed-off-by: arspauld <ars0043@uah.edu>

---
+++ teensy-sampling/matlab_proc.m
@@ -0,0 +1,101 @@
+%% CPE 621 - EMG Muscle Fatigue Monitor
+% Alex Spaulding
+% Dr. Emil Jovanov
+% November 16, 2021
+
+% Clean up output from previous runs
+clc; 
+clear; 
+close all;  
+
+%% Load EMG Data
+
+Fs = 1024;
+LOG = csvread('log6.csv');
+LOG = LOG(1:10*Fs);
+% LOG = LOG - mean(LOG);
+figure();
+subplot(2, 1, 1)
+plot(LOG), title 'Raw EMG Data', xlabel 'Index', ylabel 'Magnitude', axis([ 0 10240 min(LOG) max(LOG)])
+
+LOG_NORM = LOG-mean(LOG);
+MAV = zeros(1, length(LOG)/(Fs/10));
+for i=1:length(MAV)
+    MAV(i) = (1/(Fs/10)*sum((LOG_NORM((i-1)*Fs/10+1 : i*Fs/10)).^2))^(1/2);
+end
+subplot(2, 1, 2)
+plot(MAV), title 'MAV';
+
+% RMS = zeros(1, length(LOG)/(Fs));
+% for i=1:length(RMS)
+%     RMS(i) = (1/(Fs)*sum(LOG((i-1)*Fs+1 : i*Fs)));
+% end
+% subplot(3, 1, 3)
+% plot(RMS), title 'RMS';
+
+% Convert to frequency domain
+y_t = LOG(1:Fs);
+L = length(y_t);
+n = pow2(nextpow2(L));
+y_dft = fft(y_t, n);
+y_s = fftshift(y_dft);
+f = (-n/2:n/2-1)*(Fs/n);
+
+% y_s(Fs/2+1) = 0;
+
+figure()
+plot(f, abs(y_s))
+title 'Frequency Domain of y(t)'
+xlabel 'Frequency (Hz)'
+ylabel 'Magnitude'
+
+NFFT=256;FS=1024;
+
+%%
+is=1500;
+ii=is:(is+NFFT-1);
+figure
+e=detrend(LOG(ii));
+plot(e)
+F=abs(fft(e.*hanning(NFFT)));
+plot(F)
+df=Fs/NFFT;
+f=(0:NFFT-1)*df;
+plot(f-512,fftshift(F)), title 'Spectrum With Activation', xlabel 'Frequency (Hz)', ylabel 'Magnitude';
+
+%%
+is2=5400;
+ii2=is2:(is2+NFFT-1);
+figure
+subplot(2, 1, 1)
+es=detrend(LOG(ii2));
+F2=abs(fft(es.*hanning(NFFT)));
+plot(f-512,fftshift(F2)), title 'Peak1'
+
+psdx = (1/Fs*NFFT)*F2.^2;
+psdx(2:end-1) = 2*psdx(2:end-1);
+subplot(2, 1, 2);
+plot(f,10*log10(psdx))
+
+%%
+is3=9900;
+ii3=is3:(is3+NFFT-1);
+figure
+es2=detrend(LOG(ii3));
+F3=abs(fft(es2.*hanning(NFFT)));
+plot(f-512,fftshift(F3)), title 'Peak2'
+
+%%
+is4=1;
+ii4=is4:(is4+NFFT-1);
+figure
+es3=detrend(LOG(ii4));
+F4=abs(fft(es3.*hanning(NFFT)));
+% subplot(2, 1, 1)
+plot(f-512,fftshift(F4)), title 'Spectrum with No Activation', xlabel 'Frequency (Hz)', ylabel 'Magnitude';
+
+% psdx = (1/Fs*NFFT)*F4.^2;
+% psdx(2:end-1) = 2*psdx(2:end-1);
+% subplot(2, 1, 2);
+% plot(f,10*log10(psdx))
+
--- teensy-sampling/teensy_sd_sample/teensy_sd_sample.ino
+++ teensy-sampling/teensy_sd_sample/teensy_sd_sample.ino
@@ -1,5 +1,87 @@
+/*  TEENSY SD SAMPLING
+ *   
+ *   This program is designed to continuously poll
+ *   the analog input pins to record an accurate EMG 
+ *   signal from the Myoware EMG sensor. The data from
+ *   the EMG sensor will be written to the inserted SD
+ *   to be analyzed separately.
+ *   
+ *   Author: Alex Spaulding
+ *   Date: November 16, 2021
+ *   Class: CPE621 - Advanced Embedded Systems
+ *   Professor: Dr. Emil Jovanov
+ *   
+ *   EMG_PIN - pin 14 (A0)
+ * 
+ */
+
+#include <ADC.h>
+#include <ADC_util.h>
+#include <SD.h>
+
+#define BUFF_SIZE 10240
+#define EMG_PIN A0
+#define EMG_FREQ 1024
+#define chipSelect BUILTIN_SDCARD
+#define FFT_SIZE 256
+
+#define isr_poll_pin 26
+
+ADC *adc = new ADC();
+
+uint16_t ADC0_BUFFER[BUFF_SIZE];
+uint16_t isr_poll_flag = HIGH;
+uint32_t adc_count = 0;
+uint16_t write_needed = 1;
+
+uint32_t fft_sample_count = 0;
+uint32_t fft_buff_0[FFT_SIZE];
+uint32_t fft_buff_1[FFT_SIZE];
+uint32_t *fft_buff_active;
+
+uint32_t mav_sample_count = 0;
+uint32_t mav_buff_0[FFT_SIZE];
+uint32_t mav_buff_1[FFT_SIZE];
+uint32_t *mav_buff_active;
+
 void setup() {
   // put your setup code here, to run once:
+
+  // Initialize serial for debugging
+  Serial.begin(115200);
+  while(!Serial)
+  {
+    ;  
+  }
+
+  Serial.println("Initializing SD card...");
+
+  // Try to initialize SD card
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println("card initialized.");
+
+  // Initialize ADC
+  pinMode(EMG_PIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(isr_poll_pin, OUTPUT);
+  digitalWrite(isr_poll_pin, isr_poll_flag);
+  digitalWrite(LED_BUILTIN, HIGH);
+  adc->adc0->setAveraging(0);
+  adc->adc0->setResolution(10);
+  adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_HIGH_SPEED);
+  adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_HIGH_SPEED);
+
+  adc->adc0->stopTimer();
+  adc->adc0->startSingleRead(EMG_PIN);
+  adc->adc0->enableInterrupts(adc0_isr);
+  adc->adc0->startTimer(EMG_FREQ);
+
 
 }
 
@@ -7,3 +89,70 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+void adc0_isr()
+{
+
+  isr_poll_flag = !isr_poll_flag;
+  digitalWrite(isr_poll_pin, isr_poll_flag);
+  String data = "";
+  uint16_t adc_val = adc->adc0->readSingle();
+  if(adc_count < BUFF_SIZE)
+  {
+    ADC0_BUFFER[adc_count++] = adc_val;
+  } else if (write_needed)
+  {
+    write_needed = 0;
+    File dataFile = SD.open("log8.csv", FILE_WRITE);
+    for(int i=0; i<BUFF_SIZE; i++)
+      dataFile.println(String(ADC0_BUFFER[i]));
+    digitalWrite(LED_BUILTIN, LOW);
+    dataFile.close();
+    //Serial.println(String(adc_val));
+  }
+  
+  
+  asm("DSB");  
+}
+
+/*
+// ISR to fill FFT and MAV Ping Pong buffers
+void adc0_isr()
+{
+  // Read the current sample value
+  unsigned int sample = adc->adc0->readSingle();
+  
+  // Swap buffers if sample count is full
+  if(fft_sample_count == FFT_SIZE)
+  {
+    fft_buff_active = (fft_buff_active == fft_buff_0) ? fft_buff_1 : fft_buff_0;
+    fft_sample_count = 0;
+  }
+
+  // Read the adc value into the active buffer
+  fft_buff_active[fft_sample_count++] = sample;
+
+  // Swap buffers if sample count is full
+  if(mav_sample_count == MAV_SIZE)
+  {
+    mav_buff_active = (mav_buff_active == mav_buff_0) ? mav_buff_1 : mav_buff_0;
+    mav_sample_count = 0;
+  }
+
+  // Read the adc value into the active buffer
+  mav_buff_active[mav_sample_count++] = sample;
+}*/
+
+/*
+unsigned int calculate_MAV(unsigned int &buff, unsigned int size, unsigned int sampling frequency)
+{
+  unsigned int mean = 0;
+  for (int i = 0; i<size; i++)
+  {
+    mean += buff[i];
+  }
+  mean = mean/size;
+
+  int norm_buff[size];
+  
+}*/
